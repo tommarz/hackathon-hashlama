8eaa26fd410c94989c33e2c6a64c76414f518b25
Lior Meyer <liormeyer@gmail.com>
2021-05-19 19:16:58 -0700

    Add api/ support to fetch

--- hackathon/vue.config.js
+++ hackathon/vue.config.js
@@ -2,7 +2,14 @@ module.exports = {
     devServer: {
         watchOptions: {
             poll: true
-      }
+        },
+        proxy: {
+            '^/api': {
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+                logLevel: 'debug',
+                pathRewrite: { '^/api': '/' },
+            },
+        },
     }
-  }
-
+}
